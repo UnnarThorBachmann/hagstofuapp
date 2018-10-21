a976b8309ffd0cfde4efe20332873f71432cb4b6
Unnar Þór Bachmann <uthb@hotmail.com>
2018-10-21 16:21:46 +0000

    feat: Adding chart.js to plot line plots for comparison.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Artal from './components/Artal.js'
-import Flokkur from './components/Flokkur.js'
-import Laun from './components/Laun.js'
+import Artal from './components/Artal.js';
+import Flokkur from './components/Flokkur.js';
+import Laun from './components/Laun.js';
+import {Line} from 'react-chartjs-2';
 
 import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 import {d, flokkar} from './helpers/index.js';
@@ -42,9 +43,59 @@ class App extends Component {
   
   render() {
     const {artal,valdir,laun} = this.state;
-    console.log(this.state);
-    console.log(d[artal]);
+    const kennarar = "Kennsla á framhaldsskólastigi";
+    const kennarar_nr = 2320;
     console.log('valdir',{...valdir});
+    
+    const valdir_listi = Object.keys(valdir).reduce((acc,curr)=>{
+
+      return acc.concat(valdir[curr].map(item=>{
+        return {0: curr, 1: item}}));
+    },[]);
+    const labels = ["q1", "midgildi", "medaltal", "q3"] ;
+    console.log(valdir_listi);
+    const datasets0= [{
+          label: `${artal}-${laun}-${kennarar}` ,
+          data: labels.map(item=> d[artal][kennarar_nr][laun][item]),
+          lineTension: 0.3,
+          fill: true,
+          borderColor: 'red',
+          pointBorderColor: 'red',
+          pointRadius: 5,
+          pointHoverRadius: 15,
+          pointHitRadius: 30,
+          pointBorderWidth: 2,
+          pointStyle: 'circle'
+        }];
+    console.log(datasets0);
+    const datasets = valdir_listi.reduce((acc,curr)=> {
+      console.log(d[artal]);
+      console.log(flokkar);
+      console.log(curr);
+      return acc.concat(
+        [{
+          label: `${artal}-${laun}-${curr[1]}` ,
+          data: labels.map(item=> d[artal][flokkar[curr[0]][curr[1]]][laun][item]),
+          lineTension: 0.3,
+          fill: false,
+          borderColor: 'black',
+          pointBorderColor: 'black',
+          pointRadius: 5,
+          pointHoverRadius: 15,
+          pointHitRadius: 30,
+          pointBorderWidth: 2,
+          pointStyle: 'circle'
+        }]
+      );
+    },datasets0); 
+    console.log(datasets);    
+
+    let data= {
+        labels: labels,
+        datasets: datasets
+    }
+
+   
     return (
       <MuiThemeProvider theme={theme}>
         <Artal change={this.changeAr} artal={artal}/>
@@ -54,7 +105,7 @@ class App extends Component {
             <Flokkur key={item} name={item} flokkur={flokkar[item]} change={this.change}/>
           )  
         }
-        
+        <Line data={data} height={100} width={300} />
       </MuiThemeProvider>
     );
   }
